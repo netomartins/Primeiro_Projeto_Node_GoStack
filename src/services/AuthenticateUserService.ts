18ab9b5029e7df585ac0dd772e1efe1b9fdfc916
Neto Martins <netomartins2@hotmail.com>
2021-02-05 20:05:03 -0300

    autenticação de usuario

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,16 +1,20 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 interface Request {
   email: string;
   password: string;
-
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
 
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{user: User} > {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -25,14 +29,16 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/Password combination.');
     }
 
+    const token = sign({}, '08737222193fcfacd09ebd6069150ddb ', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
-
   }
 }
-
-
-
 
 export default AuthenticateUserService;
