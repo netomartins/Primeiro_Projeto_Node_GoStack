934fe578fc31f809ccb40cced302a4087b42abaf
Neto Martins <netomartins2@hotmail.com>
2021-02-05 18:10:45 -0300

    criando sessão de usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.router';
+import sessionsRouter from './sessions.routes';
+
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
