77d61534ebc018324529d446e59ed4ec4dc9f34b
Neto Martins <netomartins2@hotmail.com>
2021-02-05 22:53:19 -0300

    Fazendo upload

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
+
 //Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +28,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file)
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRouter;
